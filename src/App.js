51bea7bebafb01f17a6884cbf1d16885240adf75
NasserAAA <mohamed.abdelmabood@student.guc.edu.eg>
2023-03-28 05:10:05 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/About";
-import Careers from "./routes/Careers";
 import Home from "./routes/Home";
 import Navbar from './Navbar';
 function App() {
