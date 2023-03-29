7b052568e2ad82e4acce61ce5f8f14329669f40f
NasserAAA <mohamed.abdelmabood@student.guc.edu.eg>
2023-03-29 05:57:05 +0200

    setup gh-pages

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,18 @@ function HomePage() {
   return (
     <div>
       <h1>Welcome to my personal website!</h1>
-      <p>My name is Mohamed Abdelmabood and I am an AI & Automation Software Engineer at Vodafone Intelligent Solutions. I am passionate about developing innovative solutions that solve real-world problems.</p>
+      <p>My name is Mohamed Abdelmabood and I am a Software Engineer based in Cairo, Egypt. I am passionate about developing innovative solutions that solve real-world problems.</p>
+      
+      <h2>Education</h2>
+      <ul>
+        <li>Bachelor of Science - Computer Science & Engineering, German University in Cairo, 2016 - 2021</li>
+      </ul>
+      
+      <h2>Links</h2>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/mohamed-abdelmabood-5797b9212">LinkedIn</a></li>
+        <li><a href="https://github.com/NasserAAA">GitHub</a></li>
+      </ul>
     </div>
   );
 }
