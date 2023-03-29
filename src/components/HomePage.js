41a5f952e5d0ce88519f2a8d96308f03ffe414b3
NasserAAA <mohamed.abdelmabood@student.guc.edu.eg>
2023-03-29 06:08:45 +0200

    setup gh-pages

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,10 +11,12 @@ function HomePage() {
         <li>Bachelor of Science - Computer Science & Engineering, German University in Cairo, 2016 - 2021</li>
       </ul>
       
-      <h2>Links</h2>
+      <h2>Contact</h2>
       <ul>
-        <li><a href="https://www.linkedin.com/in/mohamed-abdelmabood-5797b9212">LinkedIn</a></li>
-        <li><a href="https://github.com/NasserAAA">GitHub</a></li>
+        <li><a href="mailto:mohamednabdelmabood@gmail.com">Email</a></li>
+        <li><a href="tel:+201016833660">Phone</a></li>
+        <li><a href="https://www.linkedin.com/in/mohamed-abdelmabood-5797b9212"><img src="https://image.flaticon.com/icons/svg/174/174857.svg" alt="LinkedIn" width="25" height="25"/></a></li>
+        <li><a href="https://github.com/NasserAAA"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="GitHub" width="25" height="25"/></a></li>
       </ul>
     </div>
   );
